1a35c3475176e6a34c7517622c0189399177c2c7
João Victor Pistorio <joao.pistorio@gmail.com>
2022-02-04 11:57:02 -0300

    requirement 2 implement get_most_ordered_dish_per_costumer

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,3 +1,6 @@
+from typing import Counter
+
+
 class TrackOrders:
     def __len__(self):
         return len(self.orders)
@@ -9,12 +12,17 @@ class TrackOrders:
         self.orders.append([customer, order, day])
 
     def get_most_ordered_dish_per_costumer(self, customer):
+        most_ordered_dish = [
+            order[1] for order in self.orders
+            if customer == order[0]
+        ]
+
+        return Counter(most_ordered_dish).most_common(1)[0][0]
+
+    def get_never_ordered_per_costumer(self, customer):
         pass
 
-    def get_never_ordered_per_costumer(self, costumer):
-        pass
-
-    def get_days_never_visited_per_costumer(self, costumer):
+    def get_days_never_visited_per_costumer(self, customer):
         pass
 
     def get_busiest_day(self):
