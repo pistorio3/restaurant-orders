6db02a10269e6e8903467bf7fded0acf5faea0ad
João Victor Pistorio <joao.pistorio@gmail.com>
2022-02-04 11:18:27 -0300

    requirement 2 implement add_new_order

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,11 +1,17 @@
+from typing import Counter
+
+
 class TrackOrders:
     def __len__(self):
-        pass
+        return len(self.orders)
+    
+    def __init__(self):
+        self.orders = []
 
-    def add_new_order(self, costumer, order, day):
-        pass
+    def add_new_order(self, customer, order, day):
+        self.orders.append([customer, order, day])
 
-    def get_most_ordered_dish_per_costumer(self, costumer):
+    def get_most_ordered_dish_per_costumer(self, customer):
         pass
 
     def get_never_ordered_per_costumer(self, costumer):
@@ -16,6 +22,7 @@ class TrackOrders:
 
     def get_busiest_day(self):
         pass
+
 
     def get_least_busy_day(self):
         pass
